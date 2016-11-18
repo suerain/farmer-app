aaa7d435bf13990ead72964c83afbf6622184a62
suerain <surendralamichhane07@gmail.com>
2016-11-18 16:27:51 -0600

    fourthcommit

--- src/main/java/edu/mum/farmer/app/domain/Product.java
+++ src/main/java/edu/mum/farmer/app/domain/Product.java
@@ -1,6 +1,14 @@
 package edu.mum.farmer.app.domain;
 
 public class Product {
-	
+	private double price;
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 }
