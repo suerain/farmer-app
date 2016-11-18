03568d5e122363afb1169ed6145816ce31595e50
suerain <surendralamichhane07@gmail.com>
2016-11-18 16:23:12 -0600

    thirdcommit

--- src/main/java/edu/mum/farmer/app/domain/Order.java
+++ src/main/java/edu/mum/farmer/app/domain/Order.java
@@ -1,5 +1,78 @@
 package edu.mum.farmer.app.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+	private List<OrderLine> orderLineList = new ArrayList<OrderLine>();
+	private double totalPrice;
+
+	public void addProduct(Product p) {
+		if (!getProductList().contains(p)) {
+			OrderLine oL = new OrderLine(p);
+			orderLineList.add(oL);
+		} else {
+			OrderLine oL = getOrderLineForProduct(p);
+			oL.setQuantity(oL.getQuantity() + 1);
+		}
+	}
+
+	public void removeProduct(Product p) {
+		if (getProductList().contains(p)) {
+			OrderLine oL = getOrderLineForProduct(p);
+			if (oL.getQuantity() > 1) {
+				oL.setQuantity(oL.getQuantity() - 1);
+			} else {
+				orderLineList.remove(oL);
+			}
+		}
+	}
+
+	public List<Product> getProductList() {
+		List<Product> orderList = new ArrayList<Product>();
+		for (OrderLine oL : orderLineList) {
+			orderList.add(oL.getProduct());
+		}
+		return orderList;
+	}
+
+	private OrderLine getOrderLineForProduct(Product p) {
+		if (getProductList().contains(p)) {
+			for (OrderLine oL : orderLineList) {
+				if (oL.getProduct().equals(p)) {
+					return oL;
+				}
+			}
+		}
+		return null;
+	}
+
+	public List<OrderLine> getOrderLineList() {
+		return orderLineList;
+	}
+
+	public void setOrderLineList(List<OrderLine> orderLineList) {
+		this.orderLineList = orderLineList;
+	}
+
+	public double getPrice() {
+		totalPrice = 0;
+		for (OrderLine oL : orderLineList) {
+			totalPrice += oL.getQuantity() * oL.getProduct().getPrice();
+		}
+		return totalPrice;
+	}
+
+	public double getTotalPrice() {
+		totalPrice = 0;
+		for (OrderLine oL : orderLineList) {
+			totalPrice += oL.getQuantity() * oL.getProduct().getPrice();
+		}
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
 
 }
--- src/main/java/edu/mum/farmer/app/domain/OrderLine.java
+++ src/main/java/edu/mum/farmer/app/domain/OrderLine.java
@@ -1,5 +1,31 @@
 package edu.mum.farmer.app.domain;
 
-public class OrderLine {
+import javax.persistence.Entity;
 
+@Entity
+public class OrderLine {
+	private Product product;
+	private int quantity;
+	
+	public OrderLine(Product product) {
+		this.product = product;
+		this.quantity = 1;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
 }
