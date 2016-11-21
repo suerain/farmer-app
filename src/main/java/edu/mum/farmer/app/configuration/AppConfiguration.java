ed30fbdec1f0d8032297864cf06e142c7c4e0b69
Dipen Lama <lamadipen@yahoo.com>
2016-11-21 11:28:37 -0600

    commit rest delete link

--- src/main/java/edu/mum/farmer/app/configuration/AppConfiguration.java
+++ src/main/java/edu/mum/farmer/app/configuration/AppConfiguration.java
@@ -27,7 +27,7 @@ public class AppConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/dashboard").permitAll().anyRequest().authenticated().
+		http.authorizeRequests().antMatchers("/", "/dashboard","/delete_product_r").permitAll().anyRequest().authenticated().
 
 				and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
 	}
